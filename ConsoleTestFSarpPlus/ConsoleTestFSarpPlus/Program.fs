91b6b3ca2a4710d15ea91811ef921aad9bd8fd27
jupiterfirepower <olehp78@gmail.com>
2020-05-10 10:45:18 +0300

    fix image commit

---
+++ ConsoleTestFSarpPlus/ConsoleTestFSarpPlus/Program.fs
@@ -0,0 +1,22 @@
+﻿open FSharpPlus
+
+[<EntryPoint>]
+let main _ =
+    //Most containers are functors
+    let timer = new System.Diagnostics.Stopwatch()
+    timer.Start()
+    let _ = List.map (fun x -> string (x + 10)) [ 1..100_000 ]
+    printfn "List.map Elapsed Time: %A" timer.Elapsed
+
+    let timer = new System.Diagnostics.Stopwatch()
+    timer.Start()
+    let _ = map (fun x -> string (x + 10)) [ 1..1000_000 ]
+    printfn "FSharpPlus map Elapsed Time: %A" timer.Elapsed
+
+    let timer = new System.Diagnostics.Stopwatch()
+    timer.Start()
+    let _ = Seq.map (fun x -> string (x + 10)) [ 1..1000_000 ]
+    printfn "Seq.map Elapsed Time: %A" timer.Elapsed
+
+    0 // return an integer exit code
+
